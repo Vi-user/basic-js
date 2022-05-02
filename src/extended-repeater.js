5aee6db85ecff7e1d029ecfdaaa087f7df462bc3
Vi-user <vi.volkova.2712@gmail.com>
2022-05-03 02:30:39 +0300

    extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -6,7 +6,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @param {String} str string to repeat
  * @param {Object} options options object 
  * @return {String} repeating string
- * 
+ *
  *
  * @example
  * 
@@ -15,9 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+// function repeater(/* str, options */) {
+function repeater(str, options) {
+
+  str = String(str);
+  let res = [];
+  let additionalSep = '';
+  let simpleSep = (options.separator) ? options.separator : '+';
+  const mainRepeat = (options.repeatTimes) ? options.repeatTimes : 1;
+  let additionalSeparator = (`${options.addition}` !== 'undefined') ? `${options.addition}` : '';
+  if (options) {
+    let addSep = [];
+    if (additionalSeparator) {
+      let additionRepeatTimes = (options.additionRepeatTimes) ? options.additionRepeatTimes : 1;
+      let additionSeparator = (options.additionSeparator) ? options.additionSeparator : '|';
+      for (let i = 0; i < additionRepeatTimes; i++) {
+        addSep.push(additionalSeparator);
+      }
+      additionalSep = addSep.join(additionSeparator);
+    }
+  }
+
+  str += additionalSep;
+
+  for (let i = 0; i < mainRepeat; i++) {
+    res.push(str);
+  }
+  return res.join(simpleSep);
+
 }
 
 module.exports = {
