80d5897dd052cb38217e8aa6a894e43e4fdd8655
Vi-user <vi.volkova.2712@gmail.com>
2022-05-23 17:17:35 +0300

    vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
+  constructor(type) {
+    this.type = type;
+  }
+
   encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // console.log('encrypt');
+    if (arguments.length < 2 || (typeof arguments[0] != 'string')) {
+      throw new Error('Incorrect arguments!')
+    }
+    const str = arguments[0].toUpperCase();
+    const koef = Math.ceil(str.length/arguments[1].length);
+    const key = arguments[1].repeat(koef).toUpperCase();
+    let keyCounter = 0;
+    let result = [];
+    // console.log(str);
+    // console.log(koef);
+    // console.log(key);
+    for (let i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+        const letterNum = (key.charCodeAt(keyCounter)-65) + (str.charCodeAt(i)-65) + 65;
+        // console.log(letterNum)
+        if (letterNum > 90) {
+          result.push(String.fromCharCode(letterNum-26));
+        } else {
+          result.push(String.fromCharCode(letterNum));
+        }
+        keyCounter++;
+      } else {
+        result.push(str[i])
+      }
+    }
+
+    if (this.type === false) {
+      return result.reverse().join('');
+    }
+
+    return result.join('');
   }
   decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // console.log('DEcrypt');
+    if (arguments.length < 2 || typeof arguments[0] != 'string') {
+      throw new Error('Incorrect arguments!')
+    }
+    const str = arguments[0];
+    const koef = Math.ceil(str.length/arguments[1].length);
+    const key = arguments[1].repeat(koef).toUpperCase();
+    let keyCounter = 0;
+    let result = [];
+    for (let i = 0; i < str.length; i++) {
+      if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+        const letterNum = (str.charCodeAt(i)-65) - (key.charCodeAt(keyCounter)-65) + 65;
+        // console.log(letterNum)
+        if (letterNum < 65) {
+          result.push(String.fromCharCode(letterNum + 26));
+        } else {
+          result.push(String.fromCharCode(letterNum));
+        }
+        keyCounter++;
+      } else {
+        result.push(str[i])
+      }
+      // console.log(result);
+    }
+
+    if (this.type === false) {
+      return result.reverse().join('');
+    }
+
+
+    return result.join('');
   }
 }
 
